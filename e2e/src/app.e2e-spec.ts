3db21d21af347373e4829d060a7a3b277024a9ea
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-05-14 12:22:44 -0300

    rename project and update references

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('crud-angular app is running!');
+    expect(await page.getTitleText()).toEqual('crud-mvc-angular app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-angular'`, () => {
+  it(`should have as title 'crud-mvc-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-angular');
+    expect(app.title).toEqual('crud-mvc-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('crud-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('crud-mvc-angular app is running!');
   });
 });
