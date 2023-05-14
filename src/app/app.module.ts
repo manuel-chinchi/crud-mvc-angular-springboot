aedea7aeec03f16547595cbd8e62283297e5b0c8
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-05-14 16:39:32 -0300

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent // Custom components
+    AppComponent, // Custom components
+    HeaderComponent
   ],
   imports: [
     BrowserModule // NgIf, NgFor...
