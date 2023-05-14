0c7e5bef5dd306a670d3122813efd22d00d03f5c
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-05-14 19:47:30 -0300

    update struct folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './app/components/header/header.component';
-import { FooterComponent } from './app/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
