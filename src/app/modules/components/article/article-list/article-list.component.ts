3e0e55d4fada9500d331e77daa78f2565c1f2657
manuel-chinchi <manuel.chinchi93@gmail.com>
2023-05-16 02:33:30 -0300

    add json test data

--- src/app/modules/components/article/article-list/article-list.component.ts
+++ src/app/modules/components/article/article-list/article-list.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
+import { ARTICLES } from '../articles.json';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html'
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
 
-  public articles: Article[] = [
-    { id: 1, name: 'zapatilla', description: 'nike, talle 44', quantity: 100 },
-    { id: 2, name: 'remera', description: 'adidas, talle M', quantity: 100 },
-    { id: 3, name: 'boxer', description: 'hombre', quantity: 100 }
-  ];
+  public articles: Article[];
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.articles = ARTICLES;
+  }
 }
---
+++ src/app/modules/components/article/articles.json.ts
@@ -0,0 +1,28 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+    { id: 1, name: 'camisa deportiva', description: 'Adidas, poliester', quantity: 90 },
+    { id: 2, name: 'pantalon deportivo', description: 'Adidas, poliester 95%', quantity: 120 },
+    { id: 3, name: 'calzones boxer', description: 'Calvin Klein', quantity: 100 },
+    { id: 4, name: 'calzones boxer', description: 'Kevingston', quantity: 50 },
+    { id: 5, name: 'jean', description: 'Nike', quantity: 150 },
+    { id: 6, name: 'jean', description: 'Gucci', quantity: 180 },
+
+    { id: 7, name: 'martillo neumatico', description: 'Bosch', quantity: 120 },
+    { id: 8, name: 'desmalezadora', description: 'Makita, año 2010', quantity: 120 },
+    { id: 9, name: 'amoladora', description: 'Stanley, año 2015', quantity: 30 },
+    { id: 10, name: 'desmalezadora', description: 'Hyundai', quantity: 60 },
+
+    { id: 11, name: 'heladera', description: 'Gaffa', quantity: 60 },
+    { id: 12, name: 'lavarropas', description: 'Electroluz', quantity: 40 },
+    { id: 13, name: 'cocina', description: 'General Electric', quantity: 50 },
+    { id: 14, name: 'heladera', description: 'LG, año 2012', quantity: 30 },
+    { id: 15, name: 'heladera', description: 'LG, reacondicionadas/nuevas', quantity: 100 },
+    { id: 16, name: 'lavavajillas', description: 'Sanyo, año 2005', quantity: 20 },
+
+    { id: 17, name: 'consola xbox', description: 'Microsoft, nuevas', quantity: 200 },
+    { id: 18, name: 'sega', description: 'modelo Genesis 3, a reparar/nuevas', quantity: 80 },
+    { id: 19, name: 'consola playstation 2', description: 'original, a reparar/nuevas', quantity: 120 },
+    { id: 20, name: 'lapiz 3D impresora', description: 'generico', quantity: 100 },
+    { id: 21, name: 'monopoli', description: 'Hasbro, año 2000', quantity: 30 }
+];
